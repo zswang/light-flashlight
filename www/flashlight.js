c939de5b9c6cc2dbebeb01c0c516d7843b4461c3
Zswang <wjhu111@21cn.com>
2014-04-16 19:01:16 +0800

    添加开关切换

--- www/flashlight.js
+++ www/flashlight.js
@@ -3,16 +3,51 @@ var cordova = require('cordova'),
 
 var flashlight = flashlight || {};
 
-flashlight.torch = function(successCallback, errorCallback) {
-  exec(successCallback, errorCallback, 'Flashlight', 'torch', []);
+/**
+ * 打开闪光灯
+ * @param successCallback
+ * @param errorCallback
+ */
+function torch(successCallback, errorCallback) {
+  	exec(successCallback, errorCallback, 'Flashlight', 'torch', []);
 };
 
-flashlight.off = function(successCallback, errorCallback) {
-  exec(successCallback, errorCallback, 'Flashlight', 'off', []);
+/**
+ * 关闭闪光灯
+ * @param successCallback
+ * @param errorCallback
+ */
+function off(successCallback, errorCallback) {
+	exec(successCallback, errorCallback, 'Flashlight', 'off', []);
 };
 
-flashlight.getStatus = function(successCallback, errorCallback) {
-  exec(successCallback, errorCallback, 'Flashlight', 'off', []);
+/**
+ * 判断闪光灯是否打开
+ * @param successCallback
+ * @param errorCallback
+ */
+function isTorch(successCallback, errorCallback) {
+  	exec(successCallback, errorCallback, 'Flashlight', 'isTorch', []);
 };
+
+/**
+ * 开关切换
+ * @param successCallback
+ * @param errorCallback
+ */
+function toggle(successCallback, errorCallback) {
+	isTorch(function(data) {
+		if (data.result) {
+			off(successCallback, errorCallback);
+		} else {
+			torch(successCallback, errorCallback);
+		}
+	}, errorCallback);
+};
+
+flashlight.torch = torch;
+flashlight.off = off;
+flashlight.isTorch = isTorch;
+flashlight.toggle = toggle;
 
 module.exports = flashlight;
